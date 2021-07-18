63163b40e9d2881002e45a68360d74a4f4b2dcc5
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2021-07-18 20:27:59 +0100

    Add top level try/catch to log error message and exit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { groupByAnagram, stringify, readWordsOfSameLength } from "./anagram";
 
 (async () => {
-    const [filename] = process.argv.slice(2, 3);
-    for await (const wordsOfSameLength of readWordsOfSameLength(filename)) {
-        process.stdout.write(stringify(groupByAnagram(wordsOfSameLength)));
+    try {
+        const [filename] = process.argv.slice(2, 3);
+        for await (const wordsOfSameLength of readWordsOfSameLength(filename)) {
+            process.stdout.write(stringify(groupByAnagram(wordsOfSameLength)));
+        }
+    } catch (e) {
+        console.error(e.message);
+        process.exit(1);
     }
 })();
